4d3d5f498f1610cc46727badb9c0372ea45f0117
Alphawolf781 <vincent.wambua@student.moringaschool.com>
2024-08-29 16:39:57 +0300

    completed lab

--- lib/person.py
+++ lib/person.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 
+# List of approved jobs
 APPROVED_JOBS = [
     "Admin",
     "Customer Service",
@@ -16,4 +17,30 @@ APPROVED_JOBS = [
 ]
 
 class Person:
-    pass
+    def __init__(self, name='Guido', job='Sales'):
+        self.name = name  # Initialize using the property setter
+        self.job = job    # Initialize using the property setter
+
+    def get_name(self):
+        return self._name
+    
+    
+    def set_name(self, value):
+        if isinstance(value, str) and 1 <= len(value) <= 25:
+            self._name = value.title()  # Convert to title case
+        else:
+            print("Name must be string between 1 and 25 characters.")
+    
+    name = property(get_name, set_name)
+
+    def get_job(self):
+        return self._job
+    
+    
+    def set_job(self, value):
+        if value in APPROVED_JOBS:
+            self._job = value
+        else:
+            print("Job must be in list of approved jobs.")
+
+    job= property(get_job, set_job)
